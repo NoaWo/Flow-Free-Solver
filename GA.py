4e06c92081be4545c0d0fd7c60ed137d057f5a87
Shahar6 <shaharb4r@gmail.com>
2024-02-23 16:40:00 +0200

    working version, fixed the bug with abs, added some possible functionality (no squares, custom mutilation), commented out for now

--- GA.py
+++ GA.py
@@ -9,6 +9,7 @@ from eckity.genetic_operators.mutations.vector_random_mutation import BitStringV
 from eckity.genetic_operators.selections.tournament_selection import TournamentSelection
 from eckity.statistics.best_average_worst_statistics import BestAverageWorstStatistics
 from eckity.subpopulation import Subpopulation
+from board import draw_board
 
 from examples.vectorga.knapsack.knapsack_evaluator import KnapsackEvaluator, NUM_ITEMS
 
@@ -17,12 +18,12 @@ from Flow import Flow
 from FlowEvaluator import FlowEvaluator
 from FlowNPointMutation import FlowNPointMutation
 
-board2 = [[-1, 0, -2, 0, -3],
+board = [[-1, 0, -2, 0, -3],
          [0, 0, -4, 0, -5],
          [0, 0, 0, 0, 0],
          [0, -2, 0, -3, 0],
          [0, -1, -4, -5, 0]]
-board =[
+'''board =[
 [0,0,0,0,0,0,0,0,0,0],
 [0,-1,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,-3,-4,0,0],
@@ -32,21 +33,21 @@ board =[
 [0,0,-6,0,0,-9,0,-4,-1,0],
 [0,0,0,0,0,0,0,0,0,0],
 [0,-5,-10,0,-10,0,0,-5,-3,0],
-[0,0,0,0,0,0,0,-6,-2,0]]
+[0,0,0,0,0,0,0,-6,-2,0]]'''
 board_size = len(board)
 flow = Flow(board)
 evaluator = FlowEvaluator(board_size)
 algo = SimpleEvolution(
         Subpopulation(creators=GAIntVectorCreator(length=board_size * board_size, bounds=(1, board_size),
                                                   gene_creator=flow.creator),
-                      population_size=500,
+                      population_size=200,
                       # user-defined fitness evaluation method
                       evaluator=evaluator,
                       # maximization problem (fitness is sum of values), so higher fitness is better
                       higher_is_better=True,
                       # genetic operators sequence to be applied in each generation
                       operators_sequence=[
-                          VectorKPointsCrossover(probability=0.73, k=4),
+                          VectorKPointsCrossover(probability=0.7, k=2),
                           FlowNPointMutation(board_as_vector=flow.get_board_as_vector(), board_size=board_size, evaluator=evaluator,
                                              probability=0.02, n=1)
                       ],
@@ -55,8 +56,8 @@ algo = SimpleEvolution(
                           (TournamentSelection(tournament_size=4, higher_is_better=True), 1)
                       ]),
         breeder=SimpleBreeder(),
-        max_workers=1,
-        max_generation=100,
+        max_workers=30,
+        max_generation=50,
         statistics=BestAverageWorstStatistics()
     )
 
@@ -66,4 +67,5 @@ algo.evolve()
 result = algo.execute()
 solved_board = np.array(result).reshape((board_size, board_size))
 
-print(solved_board)
+draw_board(solved_board)
+
