1b069434e23d63bbb65cd3b68c44d6cc3c03b0fe
Shahar6 <shaharb4r@gmail.com>
2024-03-02 13:33:23 +0200

    gradually increase generations and populations and stop when solved

--- solver_by_paths/main.py
+++ solver_by_paths/main.py
@@ -9,14 +9,17 @@ import random
 
 def main():
     size = input('Which size of board would you like? (type a number between 5 and 13): ')
+    pop = 100 if int(size) < 10 else 500
+    gens = 10 if int(size) < 10 else 50
+    gens_inc = int(gens * 0.5)
+    pop_inc = int(pop * 0.5)
     board_options = boards[size]
     quit = False  # todo delete this var when implement input
     while True:
-        # todo while true : input of which board from database or quit
         input_board = board_options[random.randint(0, len(board_options) - 1)]
         if input_board == "quit" or quit:
             break
-        board = input_board  # todo convert input to board
+        board = input_board
         arc_cons = ArcConsistency(copy.deepcopy(board))
         new_board = arc_cons.convert_to_smaller_problem()
         if new_board.is_solved():
@@ -26,9 +29,14 @@ def main():
             quit = True
             continue
         else:
-            ga = FlowGA(new_board)
-            ga.run()
-            # quit = True
+            ga = FlowGA(new_board, pop, gens)
+            quit = ga.run()
+            if quit:
+                print(f'\nPuzzle solved with population: {pop} and generations: {gens}')
+            if not quit:
+                quit = input('try with more resources? enter y for yes: ') != 'y'
+            pop += pop_inc
+            gens += gens_inc
 
 
 if __name__ == "__main__":
